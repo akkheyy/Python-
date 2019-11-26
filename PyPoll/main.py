ef82a7f041cdab169b904cb325d130da0ef1be64
Allison Kennedy <allie@Allisons-MacBook-Pro.local>
2019-11-26 13:00:48 -0600

    Corrected Candidate Vote Counts

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,38 +6,31 @@ csvpath = os.path.join('election_data.csv')
 
 votes = 0
 candidate_list = []
-candidate_count = {}
-
+candidate_count = []
 
 with open("election_data.csv", "r") as in_file:
-    csv_reader = csv.DictReader(in_file)
+    csv_reader = csv.reader(in_file)
     header = next(csv_reader)
    
-
     for row in csv_reader:
         votes += 1
+        candidate = row[2]
 
-        candidate = row["Candidate"]
-        
-        if candidate not in candidate_list:
-        
+        if candidate in candidate_list:
+            candidate_index = candidate_list.index(candidate)
+            candidate_count[candidate_index] += 1
+        else:
             candidate_list.append(candidate)
+            candidate_count.append(1)
 
-            candidate_count[candidate] = 0
-
-            candidate_count[candidate] = candidate_count[candidate] + 1
-            # candidate_votes = candidate_count.get(candidate)
 
 for e in range(len(candidate_list)):
-    print(f'{candidate_list} : {candidate_count}')
+    print(f'{candidate_list[e]} : {candidate_count[e]}')
 
 print("Total Votes: " + str(votes))
     
 
-    
-
-    
-
+# -------------------------------- working-ish ^
 #The total number of votes cast
 #* A complete list of candidates who received votes
 #* The percentage of votes each candidate won
